3b7e7909eb6d031201f28250256b7f24320d0d8b
Konstantin <k.a.golovnya@gmail.com>
2024-02-01 15:57:36 +1000

    Initial Commit

---
+++ Task1221Systems/Task1221SystemsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task1221SystemsApp.swift
+//  Task1221Systems
+//
+//  Created by Konstantin on 01.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Task1221SystemsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
